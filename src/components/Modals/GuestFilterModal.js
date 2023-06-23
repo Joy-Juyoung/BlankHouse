d3cc542c15b56b695f2ba2b2c06b32079eb8053d
Youngshin Park <youngshin.park@edu.sait.ca>
2023-06-23 04:22:58 -0600

    Create filter modal

---
+++ src/components/Modals/GuestFilterModal.js
@@ -0,0 +1,259 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import CloseIcon from '@mui/icons-material/Close';
+import Modal from './Modal';
+import {
+  FilterClearBtn,
+  FilterModalContainer,
+  FilterModalMain,
+  FilterResultBtn,
+  ModalCloseBtn,
+  ModalContainer,
+  ModalFooter,
+  ModalHidden,
+  ModalMain,
+  ModalMainSection,
+  ModalMainText,
+  ModalMainTitle,
+  ModalTitle,
+  ModalTop,
+  PlaceInput,
+  PlaceInputSpan,
+  PlaceInputText,
+  PlaceTypeWrap,
+  PlaceTypeWrapper,
+  PriceBetween,
+  PriceCurrency,
+  PriceInput,
+  PriceRangeBarWrap,
+  PriceRangeInputSection,
+  PriceRangeLabel,
+  PriceRangeWrap,
+  PriceRangeWrapper,
+  PropertyWrap,
+  PropertyWrpper,
+  RoomBedOption,
+  RoomBedOptionList,
+  RoomBedOptions,
+  RoomBedWrap,
+  RoomBedWrapper,
+} from './ModalStyle';
+import HomeIcon from '@mui/icons-material/Home';
+import DomainIcon from '@mui/icons-material/Domain';
+import GiteIcon from '@mui/icons-material/Gite';
+import LocationCityIcon from '@mui/icons-material/LocationCity';
+
+const anyOptionList = ['Any', '1', '2', '3', '4', '5', '6', '7', '8+'];
+
+const GuestFilterModal = ({ modalShown, toggleModal }) => {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const [bedrooms, setBedrooms] = useState('Any');
+  const [beds, setBeds] = useState('Any');
+  const [bethrooms, setBethrooms] = useState('Any');
+
+  // const [propety, setPropety] = useState('');
+  const [house, setHouse] = useState('');
+  const [apartment, setApartment] = useState('');
+  const [guesthouse, setGuesthouse] = useState('');
+  const [hotel, setHotel] = useState('');
+  const [isPropetyClick1, setIsPropetyClick1] = useState(false);
+  const [isPropetyClick2, setIsPropetyClick2] = useState(false);
+  const [isPropetyClick3, setIsPropetyClick3] = useState(false);
+  const [isPropetyClick4, setIsPropetyClick4] = useState(false);
+
+  const handleCheckedType = (e) => {
+    console.log('checked type', e.target.value);
+  };
+
+  return (
+    <Modal
+      shown={modalShown}
+      close={() => {
+        toggleModal(false);
+      }}
+      title='Filters'
+    >
+      <ModalContainer>
+        <ModalMain>
+          <ModalMainSection>
+            <ModalMainTitle>Price range</ModalMainTitle>
+            <ModalMainText>The average nightly price is $000,000</ModalMainText>
+            <PriceRangeBarWrap>price bar</PriceRangeBarWrap>
+            <PriceRangeWrap>
+              <PriceRangeWrapper>
+                <PriceRangeLabel>min price</PriceRangeLabel>
+                <PriceRangeInputSection>
+                  <PriceCurrency>$</PriceCurrency>
+                  <PriceInput type='text' value='00000' />
+                </PriceRangeInputSection>
+              </PriceRangeWrapper>
+              <PriceBetween>-</PriceBetween>
+              <PriceRangeWrapper>
+                <PriceRangeLabel>max price</PriceRangeLabel>
+                <PriceRangeInputSection>
+                  <PriceCurrency>$</PriceCurrency>
+                  <PriceInput type='text' value='00000+' />
+                </PriceRangeInputSection>
+              </PriceRangeWrapper>
+            </PriceRangeWrap>
+          </ModalMainSection>
+
+          <ModalMainSection>
+            <ModalMainTitle>Type of place</ModalMainTitle>
+            <PlaceTypeWrap>
+              <PlaceTypeWrapper>
+                <PlaceInput
+                  type='checkbox'
+                  value='Entire Place'
+                  onChange={(e) => handleCheckedType(e)}
+                />
+                <PlaceInputText>
+                  <p>Entire Place</p>
+                  <span>A Place all to youreself</span>
+                </PlaceInputText>
+              </PlaceTypeWrapper>
+              <PlaceTypeWrapper>
+                <PlaceInput
+                  type='checkbox'
+                  value='Room'
+                  onChange={(e) => handleCheckedType(e)}
+                />
+                <PlaceInputText>
+                  <p>Room</p>
+                  <span>Your own room, plus access to shared spaces</span>
+                </PlaceInputText>
+              </PlaceTypeWrapper>
+              <PlaceTypeWrapper>
+                <PlaceInput
+                  type='checkbox'
+                  value='Shared room'
+                  onChange={(e) => handleCheckedType(e)}
+                />
+                <PlaceInputText>
+                  <p>Shared room</p>
+                  <span>
+                    A sleeping space and common areas that may be shared with
+                    others
+                  </span>
+                </PlaceInputText>
+              </PlaceTypeWrapper>
+            </PlaceTypeWrap>
+          </ModalMainSection>
+
+          <ModalMainSection>
+            <ModalMainTitle>Rooms and beds</ModalMainTitle>
+            <RoomBedWrap>
+              <RoomBedWrapper>
+                <p>Bedrooms</p>
+                <RoomBedOptionList>
+                  {anyOptionList.map((option, index) => {
+                    return (
+                      <RoomBedOption
+                        key={index}
+                        selected={option === bedrooms ? true : false}
+                        onClick={() => setBedrooms(option)}
+                      >
+                        {option}
+                      </RoomBedOption>
+                    );
+                  })}
+                </RoomBedOptionList>
+              </RoomBedWrapper>
+              <RoomBedWrapper>
+                <p>Beds</p>
+                <RoomBedOptionList>
+                  {anyOptionList.map((option, index) => {
+                    return (
+                      <RoomBedOption
+                        key={index}
+                        selected={option === beds ? true : false}
+                        onClick={() => setBeds(option)}
+                      >
+                        {option}
+                      </RoomBedOption>
+                    );
+                  })}
+                </RoomBedOptionList>
+              </RoomBedWrapper>
+              <RoomBedWrapper>
+                <p>Bethrooms</p>
+                <RoomBedOptionList>
+                  {anyOptionList.map((option, index) => {
+                    return (
+                      <RoomBedOption
+                        key={index}
+                        selected={option === bethrooms ? true : false}
+                        onClick={() => setBethrooms(option)}
+                      >
+                        {option}
+                      </RoomBedOption>
+                    );
+                  })}
+                </RoomBedOptionList>
+              </RoomBedWrapper>
+            </RoomBedWrap>
+          </ModalMainSection>
+
+          <ModalMainSection>
+            <ModalMainTitle>Property type</ModalMainTitle>
+            <PropertyWrap>
+              <PropertyWrpper
+                onClick={() => {
+                  setHouse('House');
+                  setIsPropetyClick1(!isPropetyClick1);
+                }}
+                clicked={house && isPropetyClick1 ? true : false}
+              >
+                <HomeIcon fontSize='large' />
+                <p>House</p>
+              </PropertyWrpper>
+              <PropertyWrpper
+                onClick={() => {
+                  setApartment('Apartment');
+                  setIsPropetyClick2(!isPropetyClick2);
+                }}
+                clicked={apartment && isPropetyClick2 ? true : false}
+              >
+                <DomainIcon fontSize='large' />
+                <p>Apartment</p>
+              </PropertyWrpper>
+              <PropertyWrpper
+                onClick={() => {
+                  setGuesthouse('Guesthouse');
+                  setIsPropetyClick3(!isPropetyClick3);
+                }}
+                clicked={guesthouse && isPropetyClick3 ? true : false}
+              >
+                <GiteIcon fontSize='large' />
+                <p>Guesthouse</p>
+              </PropertyWrpper>
+              <PropertyWrpper
+                onClick={() => {
+                  setHotel('Hotel');
+                  setIsPropetyClick4(!isPropetyClick4);
+                }}
+                clicked={hotel && isPropetyClick4 ? true : false}
+              >
+                <LocationCityIcon fontSize='large' />
+                <p>Hotel</p>
+              </PropertyWrpper>
+            </PropertyWrap>
+          </ModalMainSection>
+
+          {/* <ModalMainSection>
+            <ModalMainTitle>Rooms and beds</ModalMainTitle>
+            <ModalMainText>The average nightly price is $000,000</ModalMainText>
+          </ModalMainSection> 
+          */}
+        </ModalMain>
+        <ModalFooter>
+          <FilterClearBtn>Clear all</FilterClearBtn>
+          <FilterResultBtn>Show 000 stay</FilterResultBtn>
+        </ModalFooter>
+      </ModalContainer>
+    </Modal>
+  );
+};
+
+export default GuestFilterModal;
---
+++ src/components/Modals/ModalStyle.js
@@ -0,0 +1,254 @@
+import styled from 'styled-components';
+
+export const ModalContainer = styled.div`
+  max-width: 750px;
+  /* max-height: 650px; */
+  /* margin: 40px; */
+  max-height: 780px;
+`;
+
+export const ModalTop = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: 1px solid lightgray;
+  padding: 0 15px;
+  height: 60px;
+`;
+
+export const ModalTitle = styled.div`
+  font-size: 18px;
+  font-weight: 600;
+`;
+
+export const ModalCloseBtn = styled.button`
+  display: flex;
+  align-items: center;
+  border: none;
+  background: none;
+  cursor: pointer;
+`;
+
+export const ModalMain = styled.div`
+  padding: 0 20px;
+  overflow-y: scroll;
+  max-height: 650px;
+`;
+
+export const ModalMainSection = styled.div`
+  padding: 30px 0;
+  border-bottom: 1px solid lightgray;
+`;
+
+export const ModalMainTitle = styled.h1`
+  font-size: 24px;
+  margin: 0;
+  padding: 0;
+`;
+
+export const ModalMainText = styled.p`
+  margin: 0;
+  padding: 0;
+  color: gray;
+`;
+
+export const ModalFooter = styled.div`
+  height: 70px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  border-top: 1px solid lightgray;
+  padding: 0 15px;
+`;
+
+// Guest Filter
+// ----------Footer-----------------
+export const FilterClearBtn = styled.button`
+  border: none;
+  background: none;
+  text-decoration: underline;
+  cursor: pointer;
+  font-size: 14px;
+`;
+
+export const FilterResultBtn = styled.button`
+  border: none;
+  border-radius: 5px;
+  background: #333333;
+  color: #fff;
+  cursor: pointer;
+  font-size: 14px;
+  padding: 10px 15px;
+
+  &:hover {
+    background: #000;
+  }
+`;
+
+// ----------Price Range--------------
+
+export const PriceRangeBarWrap = styled.div`
+  margin: 20px;
+  min-height: 80px;
+  background: lightgray;
+`;
+
+export const PriceRangeWrap = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 0 20px;
+  justify-content: space-between;
+`;
+
+export const PriceBetween = styled.div`
+  margin: 0 10px;
+  color: gray;
+`;
+
+export const PriceRangeWrapper = styled.div`
+  border: 2px solid lightgray;
+  border-radius: 5px;
+  width: 100%;
+  cursor: text;
+  padding: 5px 10px;
+  position: relative;
+
+  &:focus-within {
+    border: 2px solid black;
+  }
+`;
+
+export const PriceRangeLabel = styled.div`
+  font-size: 11px;
+  color: gray;
+  position: absolute;
+  /* top: 5px */
+`;
+
+export const PriceRangeInputSection = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  font-size: 16px;
+`;
+
+export const PriceCurrency = styled.div`
+  margin: 20px 5px 3px 0px;
+  font-size: 16px;
+`;
+
+export const PriceInput = styled.input`
+  border: none;
+  width: 100%;
+  /* border: 1px solid red; */
+  margin: 20px 10px 3px 0px;
+  font-size: 16px;
+
+  &:focus {
+    border: none;
+    outline: none;
+  }
+`;
+
+// ----------Type of Place--------------
+export const PlaceTypeWrap = styled.div`
+  margin: 20px 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 15px;
+`;
+
+export const PlaceTypeWrapper = styled.div`
+  display: flex;
+  /* align-items: center; */
+`;
+
+export const PlaceInput = styled.input`
+  width: 25px;
+  height: 25px;
+  cursor: pointer;
+  /* border: 1px solid gray; */
+  /* outline: 1px solid lightgray; */
+  margin: 0;
+
+  &:hover {
+    border: 1px solid #000;
+  }
+`;
+
+export const PlaceInputText = styled.span`
+  display: block;
+  margin-left: 10px;
+  /* margin-bottom: 10px; */
+  line-height: 80%;
+  width: 80%;
+
+  p {
+    margin: 0;
+    padding: 0;
+    font-size: 16px;
+    margin-bottom: 8px;
+  }
+
+  span {
+    font-size: 13px;
+    color: gray;
+  }
+`;
+
+// ----------room and bes---------
+export const RoomBedWrap = styled.div``;
+
+export const RoomBedWrapper = styled.div``;
+
+export const RoomBedOptionList = styled.div`
+  display: flex;
+  align-items: center;
+  /* justify-content: space-evenly; */
+`;
+
+export const RoomBedOption = styled.div`
+  margin-right: 15px;
+  border: ${({ selected }) =>
+    selected ? '1px solid #000' : '1px solid lightgray'};
+  color: ${({ selected }) => (selected ? '#fff' : '#000')};
+  background: ${({ selected }) => (selected ? '#000' : '#fff')};
+  border-radius: 20px;
+  padding: 5px 20px;
+  cursor: pointer;
+
+  &:hover {
+    border: 1px solid #000;
+  }
+`;
+
+// ------Property type-----------
+export const PropertyWrap = styled.div`
+  margin: 20px 0;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 15px;
+`;
+
+export const PropertyWrpper = styled.div`
+  /* border: 2px solid lightgray; */
+  border-radius: 10px;
+  padding: 15px;
+  height: 90px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  cursor: pointer;
+
+  border: ${({ clicked }) =>
+    clicked ? '2px solid #000' : '2px solid lightgray'};
+  background: ${({ clicked }) => (clicked ? '#f7f7f7' : '#fff')};
+
+  p {
+    margin: 0;
+  }
+
+  &:hover {
+    border: 2px solid #000;
+  }
+`;
