999d2deffb09ab9ebb23497b7b4dfa01cdf378ab
Youngshin Park <youngshin.park@edu.sait.ca>
2023-06-23 08:02:08 -0600

    Try to fix deploy error7

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import GuestMode from './pages/GuestMode';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
+import MainPage from './pages/Main/MainPage';
 
 function App() {
   return (
     <>
+      <Header />
       <Router>
-        <GuestMode />
+        {/* <GuestMode /> */}
+        <Routes>
+          <Route path='/' element={<MainPage />} exact={true} />
+        </Routes>
       </Router>
     </>
   );
