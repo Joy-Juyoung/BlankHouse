c95afcb08595cd4575f7a6be1cd781e308bd370b
Joy-Juyoung <juyu9407@gmail.com>
2023-07-06 15:03:39 -0600

    save

---
+++ src/pages/MainHome/MainPhotoSlider.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MainPhotoSlider = () => {
+  return (
+    <div>
+      <div>
+        <div>MainPhotoSlider</div>
+      </div>
+    </div>
+  );
+};
+
+export default MainPhotoSlider;
