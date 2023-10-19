be6d6a4f13db1c477ced5c56a547dcf7ab52090b
Joy-Juyoung <juyu9407@gmail.com>
2023-10-18 20:40:29 -0600

    Modified details page

--- src/pages/Room/RoomAmenity.js
+++ src/pages/Room/RoomAmenity.js
@@ -10,51 +10,65 @@ import RestaurantIcon from '@mui/icons-material/Restaurant';
 import IronIcon from '@mui/icons-material/Iron';
 import DoorFrontIcon from '@mui/icons-material/DoorFront';
 import PoolIcon from '@mui/icons-material/Pool';
+import OutdoorGrillIcon from '@mui/icons-material/OutdoorGrill';
 import { Skeleton } from '@mui/material';
 
-const RoomAmenity = ({ amenity, loading }) => {
+const RoomAmenity = ({ roomInfo, loading }) => {
+  const numberOfTimes = 6;
+  const placeholderItems = new Array(numberOfTimes).fill(null);
+
   return (
     <>
       {loading ? (
-        <PlaceOffersList>
-          <Skeleton
-            variant='rect'
-            animation='wave'
-            sx={{
-              width: '24px',
-              height: '24px',
-              borderRadius: '10px',
-              marginRight: '15px',
-            }}
-          />
-          <span>
-            <Skeleton
-              variant='rect'
-              animation='wave'
-              sx={{
-                width: '150px',
-                height: '19px',
-                borderRadius: '10px',
-                marginRight: '15px',
-              }}
-            />
-          </span>
-        </PlaceOffersList>
+        <>
+          {placeholderItems.map((_, index) => (
+            <PlaceOffersList key={index}>
+              <Skeleton
+                variant='rect'
+                animation='wave'
+                sx={{
+                  width: '24px',
+                  height: '24px',
+                  borderRadius: '10px',
+                  marginRight: '15px',
+                }}
+              />
+              <span>
+                <Skeleton
+                  variant='rect'
+                  animation='wave'
+                  sx={{
+                    width: '150px',
+                    height: '19px',
+                    borderRadius: '10px',
+                    marginRight: '15px',
+                  }}
+                />
+              </span>
+            </PlaceOffersList>
+          ))}
+        </>
       ) : (
-        <PlaceOffersList>
-          {amenity?.name === 'Wifi' && <WifiIcon />}
-          {amenity?.name === 'Kitchen' && <KitchenIcon />}
-          {amenity?.name === 'Microwave' && <MicrowaveIcon />}
-          {amenity?.name === 'Parking' && <LocalParkingIcon />}
-          {amenity?.name === 'Tv' && <TvIcon />}
-          {amenity?.name === 'Cooking basics' && <RestaurantIcon />}
-          {amenity?.name === 'Private entrance' && <DoorFrontIcon />}
-          {amenity?.name === 'Laundry' && <LocalLaundryServiceIcon />}
-          {amenity?.name === 'BBQ grill' && <WifiIcon />}
-          {amenity?.name === 'Iron' && <IronIcon />}
-          {amenity?.name === 'Pool' && <PoolIcon />}
-          <span>{amenity?.name}</span>
-        </PlaceOffersList>
+        <>
+          {roomInfo?.amenities?.slice(0, 6).map((amenity) => {
+            return (
+              <PlaceOffersList>
+                {amenity?.name === 'Wifi' && <WifiIcon />}
+                {amenity?.name === 'Kitchen' && <KitchenIcon />}
+                {amenity?.name === 'Microwave' && <MicrowaveIcon />}
+                {amenity?.name === 'Parking' && <LocalParkingIcon />}
+                {amenity?.name === 'Tv' && <TvIcon />}
+                {amenity?.name === 'Cooking basics' && <RestaurantIcon />}
+                {amenity?.name === 'Private entrance' && <DoorFrontIcon />}
+                {amenity?.name === 'Laundry' && <LocalLaundryServiceIcon />}
+                {amenity?.name === 'BBQ grill' && <OutdoorGrillIcon />}
+                {amenity?.name === 'Iron' && <IronIcon />}
+                {amenity?.name === 'Pool' && <PoolIcon />}
+                <span>{amenity?.name}</span>
+              </PlaceOffersList>
+            );
+          })}
+        </>
       )}
     </>
   );
